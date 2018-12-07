907406e6a0cfc752d7e04e17c2154a31e0d0865e
wusehuna <1362101330@qq.com>
2018-12-07 12:53:33 +0800

    Signed-off-by: wusehuna <1362101330@qq.com>

---
+++ SD_SQList2/scripts/main.js
@@ -0,0 +1,8 @@
+/*
+* @Author: 杨晴
+* @Date:   2018-12-07 11:00:31
+* @Last Modified by:   杨晴
+* @Last Modified time: 2018-12-07 11:01:44
+*/
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
